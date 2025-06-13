9491ff414048ba13db0cc518eacc871979d2624c
Mike McCarthy <michael.mccarthy@johnlewis.co.uk>
2025-06-13 11:28:04 +0100

    TRIVIA-0001 Initial commit

---
+++ src/main/kotlin/com/timeless/triviaquiz/TriviaQuizApplication.kt
@@ -0,0 +1,11 @@
+package com.timeless.triviaquiz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TriviaQuizApplication
+
+fun main(args: Array<String>) {
+    runApplication<TriviaQuizApplication>(*args)
+}
---
+++ src/test/kotlin/com/timeless/triviaquiz/TriviaQuizApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.timeless.triviaquiz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TriviaQuizApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
